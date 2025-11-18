5cef0fc64409544d86c8b485def0f58b673f528a
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-18 23:04:34 +0530

    Constructor / Parameterized Constructor
    A constructor is a special method that initializes an object, and a parameterized constructor is a type of constructor that accepts one or more parameters. This allows you to initialize an object with specific, user-defined values at the time of its creation, rather than with default values. Unlike a default constructor, which has no parameters, a parameterized constructor is always defined by the programmer and is not automatically generated by the compiler if any other custom constructor is defined.

---
+++ Lecture 42 OOPs/constructor.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class Hero
+{
+private:
+      int health;
+
+public:
+      char level;
+
+      // Constructor
+      Hero()
+      {
+            cout << "Constructor Called " << endl;
+      }
+
+      // Parameterized Constructor
+      // Also use this keyword
+
+      Hero(int health)
+      {
+            this->health = health;
+      }
+
+      Hero(int health, char level)
+      {
+            this->level = level;
+            this->health = health;
+      }
+
+      void print()
+      {
+            cout << level << endl;
+      }
+
+      int getHealth()
+      {
+            return health;
+      }
+
+      int getLevel()
+      {
+            return level;
+      }
+
+      int setHealth(int h)
+      {
+            health = h;
+      }
+
+      int setLevel(char ch)
+      {
+            level = ch;
+      }
+};
+
+int main()
+{
+      // Object creation statically
+      Hero ramesh(10);
+      cout << "Address of ramesh is: " << &ramesh << endl;
+
+      // ramesh.getHealth();
+      ramesh.print();
+
+      // Dynamically
+      Hero *h = new Hero(11);
+      h->print();
+
+      Hero temp(22, 'B');
+      temp.print();
+
+      return 0;
+}
