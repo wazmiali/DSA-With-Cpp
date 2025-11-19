915a03d87532f716e47bddb53ca20584950f9448
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-19 21:27:24 +0530

    Add files via upload

---
+++ Lecture 43 Encapsulation/Encapsulation.cpp
@@ -0,0 +1,25 @@
+// Encapsulation 
+#include<iostream>
+using namespace std;
+
+class Student {
+
+      private:
+      string name;
+      int age;
+      int height;
+
+      public:
+      int getAge(){
+            return this->age;
+      }
+};
+
+int main(){
+
+      Student first;
+
+      cout <<"All right  "<<endl;
+
+      return 0;
+}
