305beeec980ededbb39ef0dd8bfd62d5ed0c1d41
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-23 21:39:44 +0530

    Inheritance Ambiguity
    Ambiguity in inheritance occurs when a derived class inherits members (like variables or functions) with the same name from multiple base classes, causing the compiler to be unsure which one to use. This is a common issue in multiple inheritance, especially in the "diamond problem," where a class inherits from two other classes that share a common ancestor. This can be resolved by using the scope resolution operator `::` to specify which base class's member to call, or by using virtual inheritance to ensure only one copy of the common base class is inherited.

---
+++ Lecture 43 Encapsulation/AmbiguityInheritance.cpp
@@ -0,0 +1,35 @@
+// Inheritance Ambiguity
+#include <iostream>
+using namespace std;
+
+class A {
+      public:
+
+      void func() {
+            cout<<" I am A "<<endl;
+      }
+};
+
+class B {
+      public:
+
+      void func() {
+            cout<<" I am B "<<endl;
+      }
+};
+
+class C: public A, public B {
+
+};
+
+int main(){
+
+      C obj;
+      // obj.func();
+
+      // Ambiguity
+      obj.A::func();
+      obj.B::func();
+
+      return 0;
+}
