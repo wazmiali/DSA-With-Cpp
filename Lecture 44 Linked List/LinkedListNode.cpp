ff39331c71747bfd6907d8f29ab89eaeff9ec7fe
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-24 21:54:58 +0530

    Add files via upload

---
+++ Lecture 44 Linked List/LinkedListNode.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+
+      public:
+      int data;
+      Node* next;
+
+      Node(int data)
+      {
+            this -> data = data;
+            this -> next = NULL;
+      }
+};
+
+void InsertAtHead(Node* &head, int d){
+
+      //new node create 
+      Node* temp = new Node(d);
+      temp -> next = head;
+      head = temp;
+} 
+
+void print(Node* &head){
+
+      Node* temp = head;
+      while(temp != NULL){
+            cout << temp -> data <<" ";
+            temp = temp -> next;
+      }
+      cout << endl;
+}
+
+int main(){
+
+      Node* node1 = new Node(10);
+      // cout << node1 -> data << endl;
+      // cout << node1 -> next << endl;
+
+      //head printed to node1
+      Node* head = node1;
+      print(head);
+
+      InsertAtHead(head, 12);
+
+      print(head);
+
+      return 0;
+}
