09d363dc9f6bf7daf293a0c651a33cf3ba638f78
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2026-03-04 20:52:07 +0530

    Update ImplementTwoStackInArray.cpp

--- Technical Training Questions/Stack/ImplementTwoStackInArray.cpp
+++ Technical Training Questions/Stack/ImplementTwoStackInArray.cpp
@@ -78,9 +78,11 @@ int main()
       ts.push1(20);
       ts.push2(30);
       ts.push2(40);
+       //ts.push2(40);
       cout << "Popped from Stack 1: " << ts.pop1() << endl;
       cout << "Popped from Stack 2: " << ts.pop2() << endl;
       cout << "Size of Stack 1: " << ts.size1() << endl;
       cout << "Size of Stack 2: " << ts.size2() << endl;
       return 0;
 }
+
